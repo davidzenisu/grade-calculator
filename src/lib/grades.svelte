df035fdc14a428ddbae5e6fbe2767e4d33306b01
David Hofer-Zeni <david_hofer-zeni@hotmail.com>
2023-05-17 10:07:51 +0200

    Added grade display mockup

---
+++ src/lib/grades.svelte
@@ -0,0 +1,11 @@
+<script>
+	export let maxPoints;
+</script>
+
+<div>
+    <ul>
+        <li>20-100</li>
+        <li>234</li>
+        <li>234</li>
+    </ul>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,11 @@
+<script>
+	import Grades from '$lib/grades.svelte';
+</script>
+
 <div class="flex min-h-screen flex-col items-center justify-between p-8 gap-4">
     <h1 class="text-3xl">Hello world!</h1>
+    <div><Grades></Grades></div>
+    <div class="text-3xl">Footer lol</div>
 </div>
 
 <style lang="postcss">
