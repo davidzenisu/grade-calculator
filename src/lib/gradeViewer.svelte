05b1be091a0c1c9fb6662f00bf5c2f82a32575d0
David Hofer-Zeni <david_hofer-zeni@hotmail.com>
2023-05-17 10:14:58 +0200

    Renamed grade viewer component

--- src/lib/gradeViewer.svelte
+++ src/lib/gradeViewer.svelte
@@ -1,8 +1,9 @@
 <script>
-	export let maxPoints;
+	export let maxPoints = 100;
 </script>
 
 <div>
+    <div>Max points: {maxPoints}</div>
     <ul>
         <li>20-100</li>
         <li>234</li>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,10 @@
 <script>
-	import Grades from '$lib/grades.svelte';
+	import GradeViewer from '$lib/gradeViewer.svelte';
 </script>
 
 <div class="flex min-h-screen flex-col items-center justify-between p-8 gap-4">
     <h1 class="text-3xl">Hello world!</h1>
-    <div><Grades></Grades></div>
+    <div><GradeViewer></GradeViewer></div>
     <div class="text-3xl">Footer lol</div>
 </div>
 
