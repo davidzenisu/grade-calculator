dc052a94994239c49ef48b7e921c8e3b3951c3ab
David Hofer-Zeni <david_hofer-zeni@hotmail.com>
2023-05-17 10:46:53 +0200

    Added styling

--- src/lib/gradeViewer.svelte
+++ src/lib/gradeViewer.svelte
@@ -2,10 +2,27 @@
 	export let maxPoints = 100;
 </script>
 
-<div>
+<div class="grow text-4xl overflow-auto p-8">
     <div>Max points: {maxPoints}</div>
     <ul>
         <li>20-100</li>
+        <li>234</li>
+        <li>234</li>
+        <li>234</li>
+        <li>234</li>
+        <li>234</li>
+        <li>234</li>
+        <li>234</li>
+        <li>234</li>
+        <li>234</li>
+        <li>234</li>
+        <li>234</li>
+        <li>234</li>
+        <li>234</li>
+        <li>234</li>
+        <li>234</li>
+        <li>234</li>
+        <li>234</li>
         <li>234</li>
         <li>234</li>
     </ul>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,9 +2,9 @@
 	import GradeViewer from '$lib/gradeViewer.svelte';
 </script>
 
-<div class="flex min-h-screen flex-col items-center justify-between p-8 gap-4">
+<div class="flex min-h-screen max-h-screen flex-col items-center justify-between p-8 gap-4">
     <h1 class="text-3xl">Hello world!</h1>
-    <div><GradeViewer></GradeViewer></div>
+    <GradeViewer></GradeViewer>
     <div class="text-3xl">Footer lol</div>
 </div>
 
