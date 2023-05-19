9be35490078f9c8bb3f8824cff33c48b825513fd
David Hofer-Zeni <david_hofer-zeni@hotmail.com>
2023-05-19 14:06:22 +0200

    Refactored layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,9 @@
   import "../app.css";
 </script>
 
-<slot />
+<div class="flex min-h-[100dvh] max-h-screen flex-col items-center justify-between p-8 gap-4">
+  <slot />
+  <div class="text-xl">© ovid 2023</div>
+</div>
+
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -25,37 +25,35 @@
   }
 
 </script>
-<div class="flex min-h-[100dvh] max-h-screen flex-col items-center justify-between p-8 gap-4">
-    <GradeListViewer factory={gradeFactory} maxPoints={maxScore || 0} setId={setId} ></GradeListViewer>
-	<div class="w-[28rem] max-w-full px-8">
-		<form>
-			<div class="flex flex-col gap-4">
-				<div class="flex flex-row gap-4 justify-between">
-					<select class="sel-primary w-16" bind:value={setId}>
-						{#each definitionSets as set, i}
-							<option value="{set}">{set}</option>
-						{/each}
-					</select>
-					<input class="inp-primary w-28" 
-					bind:value={maxScore}
-					use:validator={maxScore}
-					type="number" 
-					id="maxScoreInput"
-					min="0" 
-					max="1000" 
-					step="1">
-					<div></div>
-				</div>
-				<div class="flex flex-row gap-4 justify-between">
-					<button class="btn-primary flex-1">1</button>
-					<button class="btn-primary flex-1">1/2</button>
-					<button class="btn-primary flex-1">1/3</button>
-					<button class="btn-primary flex-1">1/4</button>
-				</div>
+
+<GradeListViewer factory={gradeFactory} maxPoints={maxScore || 0} setId={setId} ></GradeListViewer>
+<div class="w-[28rem] max-w-full px-8">
+	<form>
+		<div class="flex flex-col gap-4">
+			<div class="flex flex-row gap-4 justify-between">
+				<select class="sel-primary w-16" bind:value={setId}>
+					{#each definitionSets as set, i}
+						<option value="{set}">{set}</option>
+					{/each}
+				</select>
+				<input class="inp-primary w-28" 
+				bind:value={maxScore}
+				use:validator={maxScore}
+				type="number" 
+				id="maxScoreInput"
+				min="0" 
+				max="1000" 
+				step="1">
+				<div></div>
 			</div>
-		</form>
-	</div>
-    <div class="text-xl">© ovid 2023</div>
+			<div class="flex flex-row gap-4 justify-between">
+				<button class="btn-primary flex-1">1</button>
+				<button class="btn-primary flex-1">1/2</button>
+				<button class="btn-primary flex-1">1/3</button>
+				<button class="btn-primary flex-1">1/4</button>
+			</div>
+		</div>
+	</form>
 </div>
 
 <style lang="postcss">
