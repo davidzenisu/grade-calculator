b95fca033c428b5c4ba6736e33713bf6e0b7cc1f
David Hofer-Zeni <david_hofer-zeni@hotmail.com>
2023-05-19 09:19:55 +0200

    Replaced input with form and reordered ui

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,7 +13,6 @@
 		 * @param {number} value
 		 */
       update(value) {
-		console.log(value);
 		maxScore = maxScore > parseInt(node.max) || maxScore < parseInt(node.min) ? lastValue : value;
 		lastValue = maxScore;
       }
@@ -21,19 +20,23 @@
   }
 
 </script>
-
-<div class="flex min-h-screen max-h-screen flex-col items-center justify-between p-8 gap-4">
-    <input class="text-3xl text-center shadow appearance-none border rounded 
-	w-28 py-4 px-4 text-gray-700 mb-3 leading-tight 
-	focus:outline-none focus:shadow-outline" 
-	bind:value={maxScore}
-	use:validator={maxScore}
-	type="number" 
-	id="weatherId"
-	min="0" 
-	max="1000" 
-	step="1">
+<div class="flex min-h-[100dvh] max-h-screen flex-col items-center justify-between p-8 gap-4">
     <GradeListViewer maxPoints={maxScore || 0}></GradeListViewer>
+	
+	<form>
+		<input class="text-3xl text-center shadow appearance-none border rounded 
+		w-28 py-4 px-4 text-gray-700 mb-3 leading-tight 
+		focus:outline-none focus:shadow-outline" 
+		bind:value={maxScore}
+		use:validator={maxScore}
+		type="number" 
+		id="maxScoreInput"
+		min="0" 
+		max="1000" 
+		step="1">
+	</form>
+
+
     <div class="text-xl">© ovid 2023</div>
 </div>
 
