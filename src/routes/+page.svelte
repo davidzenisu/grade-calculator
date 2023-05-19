bdc01a476f0922f06989d4abd07c8a33627e77b9
David Hofer-Zeni <david_hofer-zeni@hotmail.com>
2023-05-19 14:03:46 +0200

    Refactored styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -27,25 +27,34 @@
 </script>
 <div class="flex min-h-[100dvh] max-h-screen flex-col items-center justify-between p-8 gap-4">
     <GradeListViewer factory={gradeFactory} maxPoints={maxScore || 0} setId={setId} ></GradeListViewer>
-	<form>
-		<select bind:value={setId}>
-			{#each definitionSets as set, i}
-				<option value="{set}">{set}</option>
-			{/each}
-		</select>
-		<input class="text-3xl text-center shadow appearance-none border rounded 
-		w-28 py-4 px-4 text-gray-700 mb-3 leading-tight 
-		focus:outline-none focus:shadow-outline" 
-		bind:value={maxScore}
-		use:validator={maxScore}
-		type="number" 
-		id="maxScoreInput"
-		min="0" 
-		max="1000" 
-		step="1">
-	</form>
-
-
+	<div class="w-[28rem] max-w-full px-8">
+		<form>
+			<div class="flex flex-col gap-4">
+				<div class="flex flex-row gap-4 justify-between">
+					<select class="sel-primary w-16" bind:value={setId}>
+						{#each definitionSets as set, i}
+							<option value="{set}">{set}</option>
+						{/each}
+					</select>
+					<input class="inp-primary w-28" 
+					bind:value={maxScore}
+					use:validator={maxScore}
+					type="number" 
+					id="maxScoreInput"
+					min="0" 
+					max="1000" 
+					step="1">
+					<div></div>
+				</div>
+				<div class="flex flex-row gap-4 justify-between">
+					<button class="btn-primary flex-1">1</button>
+					<button class="btn-primary flex-1">1/2</button>
+					<button class="btn-primary flex-1">1/3</button>
+					<button class="btn-primary flex-1">1/4</button>
+				</div>
+			</div>
+		</form>
+	</div>
     <div class="text-xl">© ovid 2023</div>
 </div>
 
