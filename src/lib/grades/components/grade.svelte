e301c87c0bf3084aa33fe0a8beb5dea3731dde35
David Hofer-Zeni <david_hofer-zeni@hotmail.com>
2023-05-22 14:22:02 +0200

    Refactored grade display component

---
+++ src/lib/grades/components/grade.svelte
@@ -0,0 +1,11 @@
+<script>
+    /** @type {import('$lib/grades/gradeFactory').Grade}*/
+	export let grade;
+    export let indent = 0;
+</script>
+
+
+<div class="flex flex-row px-{indent} gap-8 justify-between">
+    <div>{grade.label}:</div>
+   <div>{grade.display}</div>
+</div>
