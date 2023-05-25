f93fdb49d2f4215e5393a4c2623534a2b7128a7d
David Hofer-Zeni <david_hofer-zeni@hotmail.com>
2023-05-25 15:51:03 +0200

    Added loading screen to page transitions

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,20 +2,27 @@
   import Sidebar from "$lib/menu/components/sidebar.svelte";
   import Navbar from "$lib/menu/components/navbar.svelte";
   import "../app.css";
-  import { navigating } from '$app/stores';
+  import { navigating } from "$app/stores";
+  import LoadingSpinner from "$lib/menu/components/loadingSpinner.svelte";
 
   let open = false;
 
-  navigating.subscribe((isNavigating) => {
+  navigating.subscribe(() => {
     // always close navbar!
     open = false;
   });
 </script>
 
+{#if $navigating}
+  <LoadingSpinner />
+{/if}
 <div id="main-view" class="min-h-[100dvh] max-h-screen flex flex-col">
-  <Sidebar bind:open/>
-  <Navbar bind:sidebar={open}/>
-  <div id="content-view" class="flex-1 overflow-auto flex flex-col items-center justify-between p-6 gap-4">
+  <Sidebar bind:open />
+  <Navbar bind:sidebar={open} />
+  <div
+    id="content-view"
+    class="flex-1 overflow-auto flex flex-col items-center justify-between p-6 gap-4"
+  >
     <slot />
     <div class="text-xl">© ovid 2023</div>
   </div>
