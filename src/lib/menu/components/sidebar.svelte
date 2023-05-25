a1a6b92728f32fc6b1377bbee6747fd41895ef84
David Hofer-Zeni <david_hofer-zeni@hotmail.com>
2023-05-25 16:00:53 +0200

    Added z-index to show loading on top of menu

--- src/lib/menu/components/sidebar.svelte
+++ src/lib/menu/components/sidebar.svelte
@@ -1,8 +1,11 @@
 <script>
-	export let open = false
+	export let open = false;
 </script>
 
-<aside class="absolute w-full h-full bg-slate-600 bg-opacity-95 shadow-lg" class:open>
+<aside
+	class="absolute z-5 w-full h-full bg-slate-600 bg-opacity-95 shadow-lg"
+	class:open
+>
 	<nav class="p-8 pt-16 text-2xl flex flex-col gap-4">
 		<a class="block hover:text-slate-800" href="/">Home</a>
 		<a class="block hover:text-slate-800" href="/settings">Settings</a>
@@ -13,10 +16,10 @@
 <style>
 	aside {
 		left: -100%;
-		transition: left 0.3s ease-in-out
+		transition: left 0.3s ease-in-out;
 	}
-	
+
 	.open {
-		left: 0
+		left: 0;
 	}
 </style>
