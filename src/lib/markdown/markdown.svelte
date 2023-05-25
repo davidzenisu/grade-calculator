e372c8e36ee9e144f4690205003cc1787e2368da
David Hofer-Zeni <david_hofer-zeni@hotmail.com>
2023-05-25 11:14:32 +0200

    Removed marked warnings

--- src/lib/markdown/markdown.svelte
+++ src/lib/markdown/markdown.svelte
@@ -1,6 +1,7 @@
 <script>
     import {marked} from 'marked';
     export let content = 'No **content** to show!';
+    marked.setOptions({mangled: false, headerIds: false});
 </script>
 <div class="flex flex-col gap-4">
     {@html marked.parse(content)}
