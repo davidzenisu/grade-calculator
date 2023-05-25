93264c8d7f81c288719e22eba2f11821ff1c2e8b
David Hofer-Zeni <david_hofer-zeni@hotmail.com>
2023-05-25 15:51:14 +0200

    Added artifical load in dev environment

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,8 +1,16 @@
-import { maxScoreStore,gradeSetStore, fractionStore, hideLabelStore } from "$lib/grades/gradeLocalStores";
+import { dev } from "$app/environment";
+import { maxScoreStore, gradeSetStore, fractionStore, hideLabelStore } from "$lib/grades/gradeLocalStores";
 
-export function load({ cookies }) {
+export async function load({ cookies }) {
+
 	maxScoreStore.setStoreByCookie(cookies);
 	gradeSetStore.setStoreByCookie(cookies);
 	fractionStore.setStoreByCookie(cookies);
 	hideLabelStore.setStoreByCookie(cookies);
+
+	if (dev) {
+		return new Promise((fulfil) => {
+			setTimeout(fulfil, 1000);
+		});
+	}
 }
