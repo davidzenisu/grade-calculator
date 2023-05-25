07671ca952c0fd24a0fe53b5b72d4a16a4122740
David Hofer-Zeni <david_hofer-zeni@hotmail.com>
2023-05-25 11:04:51 +0200

    Added component to render markdown contents

---
+++ src/lib/markdown/markdown.svelte
@@ -0,0 +1,7 @@
+<script>
+    import {marked} from 'marked';
+    export let content = 'No **content** to show!';
+</script>
+<div class="flex flex-col gap-4">
+    {@html marked.parse(content)}
+</div>
