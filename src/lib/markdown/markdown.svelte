094fe42a35bf501c592bf808d66ddc7469594c8c
David Hofer-Zeni <david_hofer-zeni@hotmail.com>
2023-05-25 16:00:36 +0200

    Fixed minor issue in markdown component

--- src/lib/markdown/markdown.svelte
+++ src/lib/markdown/markdown.svelte
@@ -1,8 +1,9 @@
 <script>
-    import {marked} from 'marked';
-    export let content = 'No **content** to show!';
-    marked.setOptions({mangled: false, headerIds: false});
+    import { marked } from "marked";
+    export let content = "No **content** to show!";
+    marked.setOptions({ mangle: false, headerIds: false });
 </script>
+
 <div class="flex flex-col gap-4">
     {@html marked.parse(content)}
 </div>
