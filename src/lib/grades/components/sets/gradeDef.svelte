92cb520847c23e40aed788b927432288b164b063
David Hofer-Zeni <david_hofer-zeni@hotmail.com>
2023-05-26 15:57:12 +0200

    Added component to show grades

---
+++ src/lib/grades/components/sets/gradeDef.svelte
@@ -0,0 +1,10 @@
+<script>
+    /** @type {import('$lib/grades/gradeFactory').GradeDefinition}*/
+    export let gradeDef;
+    export let indent = 0;
+</script>
+
+<div id="grade-detail" class="flex flex-row px-{indent} gap-8 justify-between">
+    <div>{gradeDef.label}:</div>
+    <div>{`>= ${gradeDef.base} %`}</div>
+</div>
---
+++ src/lib/grades/components/sets/gradeSetPreview.svelte
@@ -0,0 +1,17 @@
+<script>
+    import { GetGradePresets } from "$lib/grades/presets/gradePresets.js";
+    import GradeDefViewer from "./gradeDefViewer.svelte";
+
+    /**
+     * @type {string}
+     */
+    export let presetId;
+    const preset = GetGradePresets().find((p) => p.id === presetId);
+    /** @type {string[]} */
+</script>
+
+{#if preset}
+    <GradeDefViewer gradeDefs={preset?.grades} />
+{:else}
+    <div>No grade set available for id {presetId}</div>
+{/if}
