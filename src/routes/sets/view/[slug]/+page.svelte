b5fe1ef67a5becc4fd63032af7903f95c8464a5f
David Hofer-Zeni <david_hofer-zeni@hotmail.com>
2023-05-26 16:05:15 +0200

    Added preview page for grade set viewer

---
+++ src/routes/sets/view/[slug]/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+    import GradeSetPreview from "$lib/grades/components/sets/gradeSetPreview.svelte";
+    /** @type {string}*/
+    export let presetId = "AT";
+</script>
+
+<GradeSetPreview {presetId} />
