3022817335c978782e34d3c204ed6daaec829615
David Hofer-Zeni <david_hofer-zeni@hotmail.com>
2023-05-27 00:20:08 +0200

    Expanded page to always show data based on slug

--- src/routes/sets/view/[slug]/+page.svelte
+++ src/routes/sets/view/[slug]/+page.svelte
@@ -1,7 +1,10 @@
 <script>
     import GradeSetPreview from "$lib/grades/components/sets/gradeSetPreview.svelte";
-    /** @type {string}*/
-    export let presetId = "AT";
+    export let data;
 </script>
 
-<GradeSetPreview {presetId} />
+<div
+    class="text-2xl flex-1 flex flex-col overflow-auto max-w-full gap-3 min-w-[15rem]"
+>
+    <GradeSetPreview presetId={data.slug} />
+</div>
