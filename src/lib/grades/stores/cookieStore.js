cfc2b653b9ebd879cbd1331f08adc7c16133e438
David Hofer-Zeni <david_hofer-zeni@hotmail.com>
2023-05-26 11:40:54 +0200

    Added method to set cookie one server side

--- src/lib/grades/stores/cookieStore.js
+++ src/lib/grades/stores/cookieStore.js
@@ -31,9 +31,22 @@ export function getCookie(key) {
  * @param {'none'|'lax'|'strict'} [sameSite='strict']
  * @param {boolean} [secure=true]
 */
-export function setCookie(key, value, maxAge=31536000, sameSite='strict', secure=!dev) {
-    if(!browser) {
+export function setCookieClient(key, value, maxAge = 31536000, sameSite = 'strict', secure = !dev) {
+    if (!browser) {
         return;
     }
-    document.cookie = `${key}=${value};max-age=${maxAge};samesite=${sameSite};${secure?'Secure':''}`;
+    document.cookie = `${key}=${value};max-age=${maxAge};samesite=${sameSite};${secure ? 'Secure' : ''}`;
+}
+
+/**
+ * @param {import("@sveltejs/kit").Cookies} cookies
+ * @param {string} key
+ * @param {string} value 
+ * @param {any} [maxAge=31536000]
+ * @param {boolean |'none' | 'lax' | 'strict' | undefined} [sameSite='strict']
+ * @param {boolean} [secure=true]
+ * @param {boolean} [httpOnly=false]
+ */
+export function setCookieServer(cookies, key, value, maxAge, sameSite = 'strict', secure = !dev, httpOnly = false) {
+    cookies.set(key, value, { maxAge: maxAge, sameSite: sameSite, secure: secure, httpOnly: httpOnly });
 }
