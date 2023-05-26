6ee09eccff0119361c29e94686c6d7bfb026cad8
David Hofer-Zeni <david_hofer-zeni@hotmail.com>
2023-05-26 15:57:12 +0200

    Added component to show grades

---
+++ src/lib/grades/components/sets/gradeDefViewer.svelte
@@ -0,0 +1,17 @@
+<script>
+    import GradeDef from "$lib/grades/components/sets/gradeDef.svelte";
+    /** @type {import('$lib/grades/gradeFactory').GradeDefinition[]}*/
+    export let gradeDefs;
+    export let indent = 0;
+    const indentIncrement = 8;
+</script>
+
+{#each gradeDefs as gradeDef, i}
+    <GradeDef {gradeDef} {indent} />
+    {#if gradeDef.children}
+        <svelte:self
+            grades={gradeDef.children}
+            indent={indent + indentIncrement}
+        />
+    {/if}
+{/each}
