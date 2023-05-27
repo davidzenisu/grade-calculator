325c370c22eb624f6b1fd5b5b061604fb8e04b2b
David Hofer-Zeni <david_hofer-zeni@hotmail.com>
2023-05-27 09:24:44 +0200

    Removed text size rule

--- src/routes/sets/view/[slug]/+page.svelte
+++ src/routes/sets/view/[slug]/+page.svelte
@@ -3,8 +3,6 @@
     export let data;
 </script>
 
-<div
-    class="text-2xl flex-1 flex flex-col overflow-auto max-w-full gap-3 min-w-[15rem]"
->
+<div class="flex-1 flex flex-col overflow-auto max-w-full gap-3 min-w-[15rem]">
     <GradeSetPreview presetId={data.slug} />
 </div>
