75ffaf3c48018a779a7b767b3895fb27f6cf8e72
David Hofer-Zeni <david_hofer-zeni@hotmail.com>
2023-05-27 09:13:23 +0200

    Added deregistering unlaod event after leaving page

--- src/lib/menu/components/unsavedChanges.svelte
+++ src/lib/menu/components/unsavedChanges.svelte
@@ -1,11 +1,13 @@
 <script>
     import { browser } from "$app/environment";
     import { beforeNavigate } from "$app/navigation";
+    import { onDestroy } from "svelte";
 
     export let unsavedChanges = true;
 
     if (browser) {
-        window.addEventListener("beforeunload", function (e) {
+        /** @param {BeforeUnloadEvent} e */
+        function confirmLeavePage(e) {
             if (!unsavedChanges) {
                 return undefined;
             }
@@ -14,8 +16,11 @@
                 "If you leave before saving, your changes will be lost.";
             (e || window.event).returnValue = confirmationMessage; //Gecko + IE
             return confirmationMessage; //Gecko + Webkit, Safari, Chrome etc.
+        }
+        window.addEventListener("beforeunload", confirmLeavePage);
+        onDestroy(() => {
+            window.removeEventListener("beforeunload", confirmLeavePage);
         });
-
         beforeNavigate((navigation) => {
             console.log("before");
             if (!unsavedChanges) {
