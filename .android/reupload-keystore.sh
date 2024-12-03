73a342a4056507132856634095cdad642eaa6cc3
David Zeni-Su <david.zeni-su@oebb.at>
2024-12-03 13:11:19 +0000

    Add script to update keystore

---
+++ .android/reupload-keystore.sh
@@ -0,0 +1,3 @@
+# execute in bubblewrap output directory
+keytool -export -rfc -keystore android.keystore -alias android -file upload_certificate.pem
+# then upload as documented in https://support.google.com/googleplay/android-developer/answer/9842756#create
